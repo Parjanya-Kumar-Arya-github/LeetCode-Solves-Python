ba46479df46375c7bec5b866656a462a96ee6776
Parjanya Kumar Arya <Parjanya789@gmail.com>
2026-07-29 21:12:37 +0530

    Create MedianSortedArray.py

---
+++ MedianSortedArray.py
@@ -0,0 +1,7 @@
+#My Solution: 
+# (since time compplexity asked is O(log(m+n)), 
+# I have used binary search to find the median of two sorted arrays)
+
+class Solution:
+
+    def findMedianSortedArrays(self, nums1, nums2):
